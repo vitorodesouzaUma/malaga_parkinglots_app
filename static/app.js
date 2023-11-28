d044e1ee4e9055f2397a1e0736e25f12cf04a5c6
Vitor <vitor.odesouza@gmail.com>
2023-11-28 17:14:17 +0100

    created application prototype

--- static/app.js
+++ static/app.js
@@ -1,24 +1,57 @@
-$(document).ready(function () {
-    // Load parking lot data from the backend
-    $.get('/api/parking-lots', function (data) {
-        // Create a Leaflet map
-        var map = L.map('map').setView([36.7194, -4.4200], 13);
+var mapInitialized = false;
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
+        function updateData() {
+            if(!mapInitialized){
+                fetch('/map')
+                    .then(response => response.json())
+                    .then(data => {
+                        if (!mapInitialized) {
+                            var map = L.map('map').setView([36.7132139, -4.4076681], 13);
 
-        // Populate the parking lots table and add markers to the map
-        data.forEach(function (lot) {
-            $('#parking-lots-table tbody').append(`
-                <tr>
-                    <td>${lot.name}</td>
-                    <td>${lot.usage}%</td>
-                </tr>
-            `);
+                            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                                attribution: '© OpenStreetMap contributors'
+                            }).addTo(map);
 
-            L.marker([lot.lat, lot.lon]).addTo(map)
-                .bindPopup(`<b>${lot.name}</b><br>Usage: ${lot.usage}%`);
-        });
-    });
-});
+                            mapInitialized = true;
+                        }
+
+                        var parkingData = data;
+
+                        if (parkingData && Array.isArray(parkingData) && parkingData.length > 0) {
+                            for (var i = 0; i < parkingData.length; i++) {
+                                if (parkingData[i].availableSpotNumber.value>0){
+                                    var latitude = parkingData[i].location.value.coordinates[1];
+                                    var longitude = parkingData[i].location.value.coordinates[0];
+                                    var spotNumber = parkingData[i].availableSpotNumber.value;
+                                    var name = parkingData[i].name?.value;
+
+                                    if (latitude !== undefined && longitude !== undefined && spotNumber !== undefined && name !== undefined) {
+                                        L.marker([latitude, longitude]).addTo(map).bindPopup(name + ": " + spotNumber + " spots available");
+
+                                        var table = document.getElementById("parking-table");
+                                        var row = table.insertRow(i + 1);
+                                        var cell1 = row.insertCell(0);
+                                        var cell2 = row.insertCell(1);
+                                        var cell3 = row.insertCell(2);
+
+                                        cell1.innerHTML = name;
+                                        cell2.innerHTML = spotNumber;
+                                        cell3.innerHTML = parkingData[i].status?.value[0] || "N/A";
+                                    }
+                                }
+                            }
+                        } else {
+                            console.log("No valid parking data received.");
+                        }
+                    })
+                    .catch(error => {
+                        console.error("Error fetching parking data:", error);
+                    });
+            }
+        }
+
+        // Initial update on page load
+        updateData();
+
+        // Schedule updates every minute
+        setInterval(updateData, 60000);
